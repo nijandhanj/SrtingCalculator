36f7d3d6dc48d2fba037e2d5881a2ae4cc424705
Nijandhan Jaganathan <niju@menielan.com>
2025-03-11 10:32:53 +0530

    React component: String calculator component implemented.

---
+++ src/components/StringCalculator.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { add } from "../utils/calculation";
+
+function StringCalculator() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(null);
+
+  const handleCalculate = () => {
+    try {
+      setResult(add(input));
+    } catch (error) {
+      setResult(error.message);
+    }
+  };
+
+  return (
+    <main className="main-container">
+      <h2>String Calculator</h2>
+
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter numbers..."
+      />
+      <button onClick={handleCalculate} type="submit">
+        Submit
+      </button>
+      {<h4>Result:{result}</h4>}
+    </main>
+  );
+}
+
+export default StringCalculator;
