765b773a4cf55e941e09a2f4786d34d07bae45f2
Nijandhan Jaganathan <niju@menielan.com>
2025-03-11 08:37:59 +0530

    Test : Test cases modified.

--- src/utils/calculation.test.js
+++ src/utils/calculation.test.js
@@ -3,3 +3,11 @@ import { add } from "./calculation";
 test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+test("returns 1 when input is '1'", () => {
+    expect(add("1")).toBe(1);
+  });
+  
+  test("returns sum of two numbers separated by comma", () => {
+    expect(add("1,5")).toBe(6);
+  });
+  
